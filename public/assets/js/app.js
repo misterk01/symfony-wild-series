5d6d3c8e64700a8eb5db964196f972b398d9c3e5
Thierry Kuntzer <thierrykuntzer@gmail.com>
2019-11-15 13:16:36 +0100

    navbar

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,11 +4,15 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+const $ = require('jquery');
 
+require('bootstrap');
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
